ac8f4fb24b9a12806da47e244b6a0293c3fb91df
Jeremy Chui <jeremy@jchui.me>
2022-02-17 13:01:18 +0000

    Added eslint

--- src/help.js
+++ src/help.js
@@ -1,28 +1,34 @@
 // @flow strict
 
-import type {CommandNames, CommandDataTableType, ColumnDataTableType} from './commands.js';
+import type {
+  CommandNames,
+  CommandDataTableType,
+  ColumnDataTableType,
+} from "./commands.js";
 
-import {COMMANDS} from './commands.js';
+import { COMMANDS } from "./commands.js";
 
-export const viewHelpPage: () => void = function(){
-    const data: Array<CommandDataTableType> = Object.keys(COMMANDS).map((command: CommandNames) => {
-        const cmdData = COMMANDS[command];
-        return {
-            name: cmdData.name, 
-            url: cmdData.url, 
-            command: command
-        };
-    });
-    const columns: Array<ColumnDataTableType> = [
-        {data: 'command', title: "Command"}, 
-        {data: 'name', title: "Name"}, 
-        {data: 'url', title: "URL"}, 
-    ];
-    // $FlowFixMe - jQuery import
-    $('#help-table').DataTable({
-        data: data,
-        columns: columns,
-        order: [[ 1, "asc" ]],
-        paging: false
-    });
-}
+export const viewHelpPage: () => void = function () {
+  const data: Array<CommandDataTableType> = Object.keys(COMMANDS).map(
+    (command: CommandNames) => {
+      const cmdData = COMMANDS[command];
+      return {
+        name: cmdData.name,
+        url: cmdData.url,
+        command: command,
+      };
+    }
+  );
+  const columns: Array<ColumnDataTableType> = [
+    { data: "command", title: "Command" },
+    { data: "name", title: "Name" },
+    { data: "url", title: "URL" },
+  ];
+  // $FlowFixMe - jQuery import
+  $("#help-table").DataTable({
+    data: data,
+    columns: columns,
+    order: [[1, "asc"]],
+    paging: false,
+  });
+};
