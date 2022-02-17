207334c0fc3927f9cc8b0c045f33870e55764e29
jchui <jchui@users.noreply.github.com>
2022-02-17 13:28:47 +0000

    deploy: e56a42e70c7a8065371618a127982ba94b49d475

--- src/commands.js
+++ src/commands.js
@@ -4,6 +4,7 @@ export type CommandType = {|
   name: string,
   url: string,
   searchurl?: string,
+  category: string,
 |};
 
 export type CommandNames =
@@ -43,90 +44,103 @@ export const COMMANDS: { [CommandNames]: CommandType } = {
     name: "Facebook",
     url: "https://facebook.com/",
     searchurl: "https://www.facebook.com/search/top/?q=",
-  },
-  m: {
-    name: "Messenger Desktop App",
-    url: "messenger://",
+    category: "Search",
   },
   mw: {
     name: "Messenger Web",
     url: "https://www.messenger.com/",
+    category: "Social Media",
   },
   wa: {
     name: "WhatsApp Desktop App",
     url: "whatsapp://",
+    category: "Social Media",
   },
   waw: {
     name: "WhatsApp Web",
     url: "https://web.whatsapp.com/",
+    category: "Social Media",
   },
   gm: {
     name: "Gmail",
     url: "https://mail.google.com/mail/u/0",
     searchurl: "https://mail.google.com/mail/u/",
+    category: "Mail",
   },
   gd: {
     name: "Google Drive",
     url: "https://drive.google.com/drive/u/0",
     searchurl: "https://drive.google.com/drive/u/",
+    category: "File Storage",
   },
   yt: {
     name: "YouTube",
     url: "https://youtube.com/",
     searchurl: "https://www.youtube.com/results?search_query=",
+    category: "Search",
   },
   gh: {
     name: "GitHub",
     url: "https://github.com/",
     searchurl: "https://www.github.com/search?q=",
+    category: "Programming",
   },
   r: {
     name: "Reddit",
     url: "https://reddit.com/",
     searchurl: "https://www.reddit.com/search?q=",
+    category: "Social Media",
   },
   l: {
     name: "Linkedin",
     url: "https://linkedin.com/",
+    category: "Social Media",
   },
   ig: {
     name: "Instagram",
     url: "https://instagram.com/",
     searchurl: "https://instagram.com/",
+    category: "Social Media",
   },
   tw: {
     name: "Twitter",
     url: "https://twitter.com/",
     searchurl: "https://twitter.com/search?q=",
+    category: "Social Media",
   },
   g: {
     name: "Google",
     url: "https://google.com/",
     searchurl: "https://www.google.com/search?q=",
+    category: "Search",
   },
   n: {
     name: "Netflix",
     url: "https://netflix.com/",
     searchurl: "https://www.netflix.com/search?q=",
+    category: "Streaming",
   },
   pv: {
     name: "Amazon Prime Video",
-    url: "https://www.amazon.com/Amazon-Video/b/?&node=2858778011&ref=dvm_MLP_ROWNA_US_1",
-    searchurl: "https://www.amazon.com/s?i=instant-video&ref=nb_sb_noss_2&k=",
+    url: "https://www.amazon.co.uk/Amazon-Video/",
+    searchurl: "https://www.amazon.co.uk/s?i=instant-video&ref=nb_sb_noss_2&k=",
+    category: "Streaming",
   },
   wiki: {
     name: "Wikipedia",
     url: "https://en.wikipedia.org",
     searchurl: "https://en.wikipedia.org/wiki/",
+    category: "Search",
   },
   kz: {
     name: "Kaizen",
     url: "http://kaizenep.com",
-    searchurl: "http://kaizenep.com",
+    category: "Medical",
   },
   DEFAULT: {
     name: "Default - Google Search",
     url: "https://google.com/",
     searchurl: "https://www.google.com/search?q=",
+    category: "Search",
   },
 };
